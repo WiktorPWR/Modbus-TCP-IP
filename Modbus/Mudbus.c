bd228aea84d6c0c7a04137e895e633c83062fd0c
Wiktor <ostrowskiwiktor9@gmail.com>
2025-03-26 11:34:04 +0100

    Male zmiany

--- Modbus/Mudbus.c
+++ Modbus/Mudbus.c
@@ -1,58 +1,55 @@
 #include <stdio.h>
 
-#include "Funckje.h"
+#include "Funcktions_modbus.h"
+#include "Errors_modbus.h"
 
 char charToHex(char input, char *output) {
     sprintf(output, "0x%X", input);
     return *output;
 }
 
-void charToBinary(char input, char *output) {
-    for (int i = 7; i >= 0; i--) {
-        output[7 - i] = (input & (1 << i)) ? '1' : '0';
-    }
-    output[8] = '\0';
-}
-
 void robienie_bajtu_modbusa(char input, char nowa_ramka[12]) {
-    char binarny_znak[9];
+    char binarny_znak[9]; // Bufor na 8-bitową reprezentację binarną + znak końca
+
+    // Konwersja bajtu wejściowego na binarną reprezentację ASCII ('0' lub '1')
     for (int i = 7; i >= 0; i--) {
         binarny_znak[7 - i] = (input & (1 << i)) ? '1' : '0';
     }
-    binarny_znak[8] = '\0';
+    binarny_znak[8] = '\0'; // Dodanie znaku końca stringa
+
+    // Dodanie bitu startu (zawsze 0) na początek ramki
     nowa_ramka[0] = '0';
+
+    // Kopiowanie 8-bitowego ciągu do ramki
     for (int i = 0; i < 8; i++) {
         nowa_ramka[i + 1] = binarny_znak[i];
     }
+
+    // Liczenie liczby jedynek dla bitu parzystości
     int licznik_jedynek = 0;
     for (int i = 0; i < 8; i++) {
         if (binarny_znak[i] == '1') {
             licznik_jedynek++;
         }
     }
+
+    // Dodanie bitu parzystości (1 dla parzystej liczby jedynek, 0 dla nieparzystej)
     nowa_ramka[9] = (licznik_jedynek % 2 == 0) ? '1' : '0';
+
+    // Dodanie bitu stopu (zawsze 1)
     nowa_ramka[10] = '1';
+
+    // Dodanie znaku końca stringa
     nowa_ramka[11] = '\0';
 }
 
+void create_PDU(int function,)
 
 
 
 int main() {
-    char testChar = 'F';
-    char hexStr[5];
-    char binStr[9];
-    char ramka[12];
-    charToHex(testChar, hexStr);
-    charToBinary(testChar, binStr);
-    robienie_bajtu_modbusa(testChar, ramka);
-    printf("Znak: %c, Hex: %s, Bin: %s, Ramka: %s\n", testChar, hexStr, binStr, ramka);
     
-    int error_code = 2;
-    printf("Modbus Error %d: %s\n", error_code, modbus_error_message(error_code));
     
-    int function_code = 3;
-    printf("Modbus Function %d: %s\n", function_code, modbus_function_name(function_code));
     
     return 0;
 }
