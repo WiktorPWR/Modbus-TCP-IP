ad065bbf99d06d374cd9965dba2b5ab995dd0616
Wiktor Jankowski Ostrowski <122821918+WiktorPWR@users.noreply.github.com>
2026-01-17 23:22:42 +0100

    Delete Modbus directory

--- Modbus/main.c
+++
@@ -1,9 +0,0 @@
-#include "D:/Pulpit/PWR/Modbus/Modbus-TCP-IP/Modbus/ADU/include/test.h"
-int main() {
-    //test_init_ADU();  // Test 1
-    test_received_frame();
-    //test_destroy_ADU();  // Test 2
-    //test_transaction_identifier_management();  // Test 3
-    printf("koniec");
-    return 0;
-}
